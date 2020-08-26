17344b8cc4adf2d373c6eb992be5783509133ec0
Chrystian Rodolfo <chrystianrz@hotmail.com>
2020-08-25 21:19:13 -0300

    fix: multiple position bugs fixed

--- src/styled/containers/form-container/index.js
+++ src/styled/containers/form-container/index.js
@@ -4,7 +4,7 @@ const FormContainer = styled.div`
   display: flex;
   background: #fff;
   border-radius: 25px;
-  padding: 50px;
+  padding: 50px 20px 20px 20px;
 `;
 
 export default FormContainer;
--- src/styled/styled-input/index.js
+++ src/styled/styled-input/index.js
@@ -6,10 +6,25 @@ const StyledInput = styled(Input)`
     padding-left: 10px;
     transition: all 0.4s;
   }
+  width: 214px;
 
   :focus::placeholder {
     opacity: 0;
     transition: 0.5s;
+  }
+`;
+
+StyledInput.Password = styled(Input.Password)`
+  width: 214px;
+
+  :focus::placeholder {
+    opacity: 0;
+    transition: 0.5s;
+  }
+
+  :hover::placeholder {
+    padding-left: 10px;
+    transition: all 0.4s;
   }
 `;
 
